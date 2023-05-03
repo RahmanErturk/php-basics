916087373b9a7041528895e58ee78fdc3c5a28ff
RahmanErturk <rahman.erturk@gmail.com>
2023-05-03 09:20:02 +0200

    save

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php 
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "firstPhpApp_db";
+
+    $connect = mysqli_connect($host, $username, $password, $dbname);
+
+    // if (!$connect) {
+    //     die("Not connected");
+    // } else {
+    //     echo "connected successfully";
+    // }
+
+    if (mysqli_connect_errno() > 0) {
+        die("Connection failed: ". mysqli_connect_errno());
+    }
+
+    echo "Connected successfully"."<br>";
+
+    mysqli_close($connect);
+
+    echo "Closed successfully";
+
+?>
